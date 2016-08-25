77018173eca802be3861d11db36e1ed20a9f79fb
julesday <julerday17@charter.newvisions.org>
2016-08-24 22:09:11 -0400

    added message for  texting file

--- www/js/texting.js
+++ www/js/texting.js
@@ -1,24 +1,21 @@
-var chat = "";
+var chat = "Translator: Hi, My name is Aaron, How may I help you?";
 $(document).ready(function(){
+	$("#chatbox").html(chat);
 //If user submits the form
 	$("#submitmsg").click(function(){	
 		var clientmsg = $("#usermsg").val();
-		
 
-		switch (clientmsg) {
-		case clientmsg == "hi": 
-		chat += "<br>"
-		chat += "clientmsg"
-		break;
-		case clientmsg == "umm": 
-		chat += "<br>"
-		chat += "clientmsg"
-}
-		
+			chat += "<br>"
+			chat += "Me: "
+			chat += clientmsg
+			switch(clientmsg){
+				case "Hi Aaron, I was wondering why these Americans keep on telling me to 'break my leg' before my presentation":
+				chat += "<br>"
+				chat += "Translator: to 'break a leg' is a common phrase used in the united states that means good luck"	
+				}
 		
 		$("#chatbox").html(chat);
 		return false;
 	});
  
 });
-
