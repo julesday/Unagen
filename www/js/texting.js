d812d68019a4b28985157925818c7c4506537ba2
julesday <julerday17@charter.newvisions.org>
2016-09-04 17:24:49 -0400

    button add text to textboc

--- www/js/texting.js
+++ www/js/texting.js
@@ -1,4 +1,4 @@
-var chat = "<span class='message'><b>Translator:</b> Hi, My name is Aaron, How may I help you?</span>";
+var chat = "<span class='message'><b>Translator:</b> Hi, My name is Aaron, How may I help you? Please state your goal for this session.</span>";
 var responses = {
   "hello": "How can I help you?",
   "I need a translation" : "No problem"
@@ -17,17 +17,30 @@ $(document).ready(function () {
       chat += "<br>";
       chat += "<span class='message'>" + me + clientMsg + "</span>";
       switch (clientMsg) {
-        case "Hi Aaron, I was wondering why these Americans keep on telling me to 'break my leg' before my presentation":
-          chat += "<br>";
-          chat += "<span class='message'>" + trans + "is a common phrase used in the united states that means good luck</span>";
-          break;
+
         case "hello":
           chat += "<br>"
           chat += "<span class='message'>" + trans + "hello, how may I help you</span>";
           break;
+        case "no thank you":
+          chat += "<br>"
+          chat += "<span class='message'>" +trans + "Okay, your session is now over."
+          break;
+        case "No thank you":
+          chat += "<br>"
+          chat += "<span class='message'>" +trans + "Okay, your session is now over."
+          break;
+        case "no, thank you":
+          chat += "<br>"
+          chat += "<span class='message'>" +trans + "Okay, your session is now over."
+          break;
+        case "No, thank you":
+          chat += "<br>"
+          chat += "<span class='message'>" +trans + "Okay, your session is now over."
+          break;
         default:
           chat += "<br>";
-          chat += "<span class='message'>" + trans + "is a common phrase used in the united states that means good luck</span>";
+          chat += "<span class='message'>" + trans + "To break a leg is a common phrase used in the united states that means good luck. Is there anything else I may help you with?</span>";
           break;
       }
 
@@ -36,3 +49,12 @@ $(document).ready(function () {
   });
 
 });
+
+//$('#add-text').click(function() {
+//    $('#userMsg').val('Hi Aaron, I was wondering why my co-workers keep on telling me to break my leg before my presentation');
+//});
+
+function add(text){
+    var TheTextBox = document.getElementById("userMsg");
+    TheTextBox.value = TheTextBox.value + text;
+}
